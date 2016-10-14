6cce0a19ef1b1ea6ebcc360d9d060ab6c57c36fc
ALI ALRAWI <ali.developer90@gmail.com>
2016-10-14 16:43:50 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -1,7 +1,31 @@
 class Song
   attr_accessor :name, :artist_name
   @@all = []
+  def self.create
+    song=self.new
+    self.all.push(song)
+    return song
+  end
+  def self.new_by_name(name_)
+    song=self.new
+    song.name=name_
+    return song
+  end
+  def self.create_by_name(name_)
+    song = self.create
+    song.name = name_
+    return song
+  end
+  def self.find_by_name(name_)
+    self.all.detect{|e| e.name == name_}
+end
+    def self.find_or_create_by_name(name_)
+      self.find_by_name(name_) || self.create_by_name(name_)
 
+  end
+  def self.alphabetical
+  self.all.sort_by { |e| e.name }
+  end
   def self.all
     @@all
   end
@@ -9,5 +33,26 @@ class Song
   def save
     self.class.all << self
   end
+def self.new_from_filename(filename)
+  song=self.new
+  arr=filename.split(" - ")
+  song.artist_name=arr[0]
+  arr2=arr[1].split(".")
+  song.name=arr2[0]
+  return song
+end
+def self.create_from_filename(filename)
+  song=self.create
+  arr=filename.split(" - ")
+  song.artist_name=arr[0]
+  arr2=arr[1].split(".")
+  song.name=arr2[0]
+  return song
+end
+def self.destroy_all
+  self.all.clear
 
 end
+end
+song=Song.new_from_filename("Thundercat - For Love I Come.mp3")
+puts song.name
