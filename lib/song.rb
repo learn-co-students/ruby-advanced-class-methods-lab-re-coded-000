91e4f36931d5dd000cd8a1b27bc608bb1483c767
Ayedh Gaser <alshmary_ad@yahoo.com>
2016-11-01 17:07:20 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -1,13 +1,68 @@
 class Song
   attr_accessor :name, :artist_name
   @@all = []
-
   def self.all
     @@all
   end
-
+# ----------------------------------------------------
   def save
     self.class.all << self
   end
-
+  # ----------------------------------------------------
+  def self.create
+    song = self.new
+    @@all << song
+    song
+  end
+  # ----------------------------------------------------
+  def self.create_by_name(song_name)
+    new_song = self.new
+    new_song.name = song_name
+    self.all << new_song
+    new_song
+  end
+  # ----------------------------------------------------
+  def self.new_by_name(song_name)
+    new_song = self.new
+    new_song.name = song_name
+    new_song
+  end
+  # ----------------------------------------------------
+    def self.find_by_name(songname)
+      self.all.detect do |song|
+        song.name == songname
+      end
+    end
+ # ----------------------------------------------------
+    def self.find_or_create_by_name(song_name)
+      self.find_by_name(song_name) || self.create_by_name(song_name)
+    end
+# ----------------------------------------------------
+    def self.new_from_filename(filename)
+        spltarr= filename.split("-")
+        spltarr2 = spltarr[1].split("mp3")
+        spltarr3 = spltarr2[0].strip.split(".")
+        song = self.new
+        song.artist_name= spltarr[0].strip
+        song.name = spltarr3[0]
+        song
+    end
+# ----------------------------------------------------
+    def self.create_from_filename(filename)
+      spltarr= filename.split("-")
+      spltarr2 = spltarr[1].split("mp3")
+      spltarr3 = spltarr2[0].strip.split(".")
+      song = self.new
+      song.artist_name= spltarr[0].strip
+      song.name = spltarr3[0]
+      self.all << song
+    end
+# ----------------------------------------------------
+    def self.alphabetical
+      self.all.sort_by{|song| song.name}
+    end
+# ----------------------------------------------------
+    def self.destroy_all
+      self.all.clear
+    end
 end
