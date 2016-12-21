925dacee7dd7a1ce655f9fe2f7eeaa1c616d7eca
MOHAMMAD DAYLAKI <mohammad.daylaky@gmail.com>
2016-12-21 03:16:06 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -10,4 +10,58 @@ class Song
     self.class.all << self
   end
 
+def self.create
+  song =self.new
+  self.all << song
+  song
+end
+
+def self.new_by_name(name_song)
+  new_song =self.new
+  new_song.name =name_song
+  new_song
+end
+
+def self.create_by_name(name_song)
+   new1_song =self.create
+   new1_song.name =name_song
+   new1_song
+end
+
+def self.find_by_name(name_song)
+  self.all.detect{|person|
+    person.name== name_song}
+end
+
+def self.find_or_create_by_name(name_song)
+self.find_by_name(name_song) ||self.create_by_name(name_song)
+end
+
+def self.alphabetical
+  self.all.sort_by{|x| x.name}
+end
+
+def self.new_from_filename(filename)
+    parts = filename.split(" - ")
+    artist_name = parts[0]
+    song_name = parts[1].gsub(".mp3", "")
+    song = self.new
+    song.name = song_name
+    song.artist_name = artist_name
+    song
+  end
+  def self.create_from_filename(filename)
+     parts = filename.split(" - ")
+     artist_name = parts[0]
+     song_name = parts[1].gsub(".mp3", "")
+
+     song = self.create
+     song.name = song_name
+     song.artist_name = artist_name
+     song
+   end
+
+   def self.destroy_all
+     self.all.clear
+   end
 end
