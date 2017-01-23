643eb766f426d3a566ca0fff78880f1754771e06
noven temer <noven5823@gmail.com>
2017-01-23 12:33:21 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -1,7 +1,11 @@
 class Song
   attr_accessor :name, :artist_name
   @@all = []
-
+  def initializes(name, artist_name)
+    @name=name
+    @artist_name=artist_name
+    @@all << self
+  end
   def self.all
     @@all
   end
@@ -9,5 +13,7 @@ class Song
   def save
     self.class.all << self
   end
-
+def self.create(name)
+  @@all.deyecat {|s| s.name == name}
+end
 end
